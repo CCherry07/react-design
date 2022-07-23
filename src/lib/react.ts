d7b900a23489e915cf974552013c7fe5cac39571
CCherry07 <2405693142@qq.com>
2022-07-23 20:17:41 +0800

    chore:  createElement type

--- src/lib/react.ts
+++ src/lib/react.ts
@@ -1,24 +1,39 @@
 import { REACT_TEXT } from "./const";
+declare module _JSX {
+  export interface Props {
+    className: string;
+    children: JSX;
+  }
 
-export function createElement(type: any,config: { __self: any; __source: any; }, ...children: any[]){
+  export interface JSX {
+    type: string;
+    key?: any;
+    ref?: any;
+    props: Props;
+    _owner?: any;
+    _store: any;
+  }
+}
+
+export function createElement(type: string, config: { __self: any; __source: any; }, ...children: _JSX.JSX[]) {
   // 删除不必要的属性，简化代码
   if (config) {
     delete config.__self;
     delete config.__source;
   }
   return {
-      type,
-      props: {
-          ...config,
-          children:children.map((child) => {
-              return typeof child === 'object'? child: {
-                  type: REACT_TEXT,
-                  props: {
-                      content: child,
-                      children: []
-                  }
-              }
-          })
-      }
+    type,
+    props: {
+      ...config,
+      children: children.map((child) => {
+        return typeof child === 'object' ? child : {
+          type: REACT_TEXT,
+          props: {
+            content: child,
+            children: []
+          }
+        }
+      })
+    }
   }
 }
